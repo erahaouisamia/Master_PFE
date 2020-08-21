5713a7f2adcf53e42febf5f3e162c36ad99dfa81
erahaouisamia <61827242+erahaouisamia@users.noreply.github.com>
2020-08-21 11:35:52 +0100

    Delete EmployeeRepository.java

--- springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package net.guides.springboot2.springboot2jpacrudexample;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
