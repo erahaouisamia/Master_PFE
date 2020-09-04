709203789e22d7481bf58d219d2ee271b95097e6
erahaouisamia <61827242+erahaouisamia@users.noreply.github.com>
2020-09-04 11:27:49 +0100

    Update Springboot2JpaCrudExampleApplication.java

--- app/crud-api/src/main/java/crudapi/Springboot2JpaCrudExampleApplication.java
+++ app/crud-api/src/main/java/crudapi/Springboot2JpaCrudExampleApplication.java
@@ -2,6 +2,7 @@ package crudapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {
   DataSourceAutoConfiguration.class
