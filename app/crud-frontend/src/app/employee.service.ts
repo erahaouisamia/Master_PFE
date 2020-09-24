4d544996599c0af136dae7904ff765cc3b34106b
erahaouisamia <61827242+erahaouisamia@users.noreply.github.com>
2020-09-24 01:23:07 +0100

    Update employee.service.ts

--- app/crud-frontend/src/app/employee.service.ts
+++ app/crud-frontend/src/app/employee.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  private baseUrl = 'https://crudapi.com/api/v1/employees';
+  private baseUrl = 'https://crudemployee.com/api/v1/employees';
 
   constructor(private http: HttpClient) { }
 
