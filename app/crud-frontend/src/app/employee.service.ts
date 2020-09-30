5937e79ec2b4eab0f9202b6c0bf678ae26590f4d
erahaouisamia <samiaerahaoui.aws@gmail.com>
2020-09-30 12:19:30 +0100

    updates

--- app/crud-frontend/src/app/employee.service.ts
+++ app/crud-frontend/src/app/employee.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EmployeeService {
 
-  private baseUrl = 'https://crudemployee.com/restapi/api/v1/employees/';
+  private baseUrl = 'https://crudemployee.com/api/v1/employees/';
 
   constructor(private http: HttpClient) { }
 
